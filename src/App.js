1e14d52a95d5571a062a7f69ec4dbc3ec11d2478
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-15 16:02:59 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,24 +3,48 @@
 function App() {
   return(
     <>
-    <MessageDemo name = "Umair" email="u@gamil.com"/>
-    <MessageDemo name="Waseem" email="w@gmail.com"/>
-    <MessageDemo name="Khan" email="k@gmail.com"/>
-    <MessageDemo id="230340320124"/>
-    
-    
-    
+    <h1>Props Demo</h1>
+    <ListDemo/>
+
     </>
   );
 }
-function MessageDemo({name, email}) {
-  return (
+
+function ListDemo() {
+  let data = "Hello Universe";
+  let list = ["", "", "", ""];
+
+  let list1 = [];
+  for (let i = 0; i < 10; i++) {
+    list1.push("");
+    
+  }
+
+
+  return(
     <>
-    <h5>Hello {name} {email}</h5>
-    {/* <h5>CDAC ID:- {props.id}</h5> */}
+      <h1>{data}</h1>
+
+      {/* {list.forEach((item) => item)} */}
+
+
+      {list.map((item) => (
+        <div>
+          <h1>Hello Universe</h1>
+        </div>
+      ))}
+
+      <hr />
+
+      {list.map((item) => (
+       <>
+       <h1>hellooouuu</h1>
+       </>
+      ))}
+
     </>
   );
 }
-  
+ 
 export default App;
 
