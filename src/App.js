858df970178c3857dd177f706a365f02390a8239
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-17 00:10:43 +0530

    DONE

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MyTodo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
 
 function App() {
   return (
     <>
-    <h1>Registration</h1>
-    <MyRegistration/>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Hello</h1>}/>
+      <Route path="/home" element={<h1>Home</h1>}/>
+      <Route path="/todo" element={<MyTodo/>}/>
+      <Route path="/registration" element={<MyRegistration/>}/>
+      <Route path="*" element={<h1>Page Not Found</h1>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
