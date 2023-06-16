f4dca1dafbdd885a6dd0d47b2233b214d7975339
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-16 18:49:54 +0530

    mytodo in separate file

---
+++ src/pages/MyTodo.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+
+function MyTodo() {
+    let [successBox, setSuccessBox] = useState(false);
+    let [todo, setTodo] = useState({task: "", description: ""});
+    
+    let handleChangeTaskAction = (e) =>{
+      let newTodo ={...todo, task: e.target.value};
+      setTodo(newTodo);
+    };
+  
+    let handleChangeTaskDescriptionAction = (e) => {
+        //console.log(e.target);
+      let newTodo ={...todo, description: e.target.value};
+      setTodo(newTodo);
+    };
+  
+    let addTodoAction = async () => {
+      console.log(todo);
+      //alert(todo.task + todo.description);
+  
+      let url = `http://localhost:4000/addtodo?task=${todo.task}&description=${todo.description}`;
+      await fetch(url);
+  
+      let newtodo = {task:"", description:""};
+      setTodo(newtodo);
+  
+      setSuccessBox(true);
+      setTimeout (() => setSuccessBox(false), 5000);
+    };
+    return (
+      <>
+       
+        <input  
+        className="form-control" 
+        type="text" 
+        placeholder="Enter Task"
+        value={todo.task} 
+        onChange={handleChangeTaskAction}
+        />
+  
+        <textarea 
+        className="form-control" 
+        cols="30" 
+        rows="3" 
+        placeholder="Eter Description"
+        value={todo.description}
+        onChange={handleChangeTaskDescriptionAction}
+        ></textarea>
+       
+        <input type="button" value="Add Todo" onClick={addTodoAction}/>
+  
+        {successBox && (
+          <div className="alert alert-success">Operation Success</div>
+        )}
+        
+      </>
+    );
+  }
+
+  export default MyTodo;
