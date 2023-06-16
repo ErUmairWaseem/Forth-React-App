72d67b9c526da417c59bcae8cd7e4438770b31df
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-16 18:15:44 +0530

    connection done

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
 import { useState } from "react";
 
-function  App() {
-  return(
+function App() {
+  return (
     <>
-       <h1>My TODO</h1>
-       <MyToDo />
+    <h1>My Todo</h1>
+    <MyTodo/>
     </>
-   
   );
 }
 
-function MyToDo() {
-
-
-  let[todo, setTodo] = useState({task : ""});
-
-  let handleTaskAction = (e) =>{
-    console.log(e.target);
-
-    let newTodo = {...todo, task: e.target.value}
-    setTodo(newTodo);
-
-  };
-
-  let addTodoAction = () => {
-    alert (todo.task)
-  };
+function MyTodo() {
+  let [todo, setTodo] = useState({task: "", description: ""});
   
+  let handleChangeTaskAction = (e) =>{
+    let newTodo ={...todo, task: e.target.value};
+    setTodo(newTodo);
+  };
 
-  return(
+  let handleChangeTaskDescriptionAction = (e) => {
+      //console.log(e.target);
+    let newTodo ={...todo, description: e.target.value};
+    setTodo(newTodo);
+  };
+
+  let addTodoAction = async () => {
+    console.log(todo);
+    //alert(todo.task + todo.description);
+
+    let url = `http://localhost:4000/addtodo?task=${todo.task}description=${todo.description}`
+    await fetch(url);
+  };
+  return (
     <>
-      <input 
+     
+      <input  
       className="form-control" 
       type="text" 
       placeholder="Enter Task"
-      value={todo.task}
-      onChange={handleTaskAction}
+      value={todo.task} 
+      onChange={handleChangeTaskAction}
       />
-      {/* <input className="form-control" type="text" placeholder="Enter Discription"/> */}
+
+      <textarea 
+      className="form-control" 
+      cols="30" 
+      rows="3" 
+      placeholder="Eter Description"
+      value={todo.description}
+      onChange={handleChangeTaskDescriptionAction}
+      ></textarea>
+     
       <input type="button" value="Add Todo" onClick={addTodoAction}/>
     </>
   );
